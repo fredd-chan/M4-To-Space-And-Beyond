800e2d309a127cdbaa570a641e9aa0a45c596cb4
fredd-chan <frchan@s.sfusd.edu>
2025-11-04 11:15:16 -0800

    M4L5

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,29 +1,29 @@
 extends Sprite2D
 
+var steering_factor := 10
 var normal_speed := 600
 var max_speed := normal_speed
 var velocity := Vector2(0,0)
 var boost_speed :=1500.0
+var direction:= Vector2(0,0)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		get_node("Timer").start()
 		
-	var direction := Vector2(0,0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
-
-
- 
 
 
 func _on_timer_timeout() -> void:
